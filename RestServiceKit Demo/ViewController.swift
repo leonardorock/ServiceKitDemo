77473c1889d9e43f41ad27169599fc176882ab92
Leonardo <leonardo_rock@icloud.com>
2017-10-06 15:14:32 -0300

    Initial Commit

---
+++ RestServiceKit Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RestServiceKit Demo
+//
+//  Created by Leonardo Oliveira on 10/6/17.
+//  Copyright © 2017 Leonardo Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RestServiceKit DemoTests/RestServiceKit_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  RestServiceKit_DemoTests.swift
+//  RestServiceKit DemoTests
+//
+//  Created by Leonardo Oliveira on 10/6/17.
+//  Copyright © 2017 Leonardo Oliveira. All rights reserved.
+//
+
+import XCTest
+@testable import RestServiceKit_Demo
+
+class RestServiceKit_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
